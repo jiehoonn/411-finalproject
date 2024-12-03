7c622b2145d873f17dcb03267576441eae86ceb1
Teresa Joseoh <teresaj@bu.edu>
2024-12-03 04:32:32 -0500

    user account routes with validation and password hashing

---
+++ app/user_service.py
@@ -0,0 +1,11 @@
+from app.models import db, User
+
+def create_user(username, email, password):
+    if User.query.filter_by(username=username).first():
+        raise ValueError("Username already exists")
+
+    user = User(username=username, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return user
