38b418248cef7f18febe52a3853be167c5aa3810
varuni24 <varunigupta24@gmail.com>
2024-12-06 15:14:31 -0500

    feat: fixed routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
+import RegisterPage from './components/userRegistration';
+import LoginPage from './components/loginPage';
+import MainPage from './components/mainPage';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Dashboard />} /> {/* Redirect home to dashboard */}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                {/* Default Route Redirects to Login */}
+                <Route path="/" element={<Navigate to="/main" />} />
+                <Route path="/main" element={<MainPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
