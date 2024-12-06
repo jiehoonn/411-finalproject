e649117dcc4897efe7bf8005ab174d704d92c9ad
varuni24 <varunigupta24@gmail.com>
2024-12-06 15:30:14 -0500

    feat: added dashboard and portfolio routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
+import Portfolio from './pages/portfolio';
 import RegisterPage from './components/userRegistration';
 import LoginPage from './components/loginPage';
 import MainPage from './components/mainPage';
@@ -9,12 +10,12 @@ function App() {
     return (
         <Router>
             <Routes>
-                {/* Default Route Redirects to Login */}
                 <Route path="/" element={<Navigate to="/main" />} />
                 <Route path="/main" element={<MainPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/portfolio" element={<Portfolio />} />
             </Routes>
         </Router>
     );
