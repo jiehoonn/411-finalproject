d05ae4c769072e537393e46699ef187844b83f0b
Teresa Joseoh <teresaj@bu.edu>
2024-12-07 09:09:48 -0500

    feat(database): define database initialization in __init__.py

--- app/backend/database/__init__.py
+++ app/backend/database/__init__.py
@@ -1,12 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# Create a SQLAlchemy instance
+# create a SQLAlchemy instance
 db = SQLAlchemy()
 
 def init_db(app):
-    """
-    Bind the database to the Flask app and create tables if they don't exist.
-    """
+    
     db.init_app(app)
     with app.app_context():
         db.create_all()
