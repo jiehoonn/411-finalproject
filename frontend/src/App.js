54d1fd1a28d56ce3492d516724eb1bf44430056e
Teresa Joseoh <teresaj@bu.edu>
2024-12-08 11:40:24 -0500

    feat: Set up main app structure with routing for login and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
 // import React from 'react';
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import StockLookup from './components/stockLookup';
@@ -16,56 +36,56 @@
 
 // export default App;
 
-import React, { useState } from "react";
-import { Layout, Card, Typography, Row, Col } from "antd";
-import HistoricalData from "./components/historicalData";
-import StockLookup from "./components/stockLookup";
-import BuyStock from "./components/buyStock";
-import SellStock from "./components/sellStock";
+// import React, { useState } from "react";
+// import { Layout, Card, Typography, Row, Col } from "antd";
+// import HistoricalData from "./components/historicalData";
+// import StockLookup from "./components/stockLookup";
+// import BuyStock from "./components/buyStock";
+// import SellStock from "./components/sellStock";
 
-const { Title } = Typography;
-const { Header, Content } = Layout;
+// const { Title } = Typography;
+// const { Header, Content } = Layout;
 
-function App() {
-  const [range, setRange] = useState("1d");
-  return (
-    <Layout>
-      <Header>
-      <Title level={2} style={{ color: "white", textAlign: "center", margin: 10 }}>
-          Stock Trading Platform
-        </Title>
-      </Header>
-      <Content style={{ padding: "50px" }}>
-        <Row gutter={[20, 20]}>
-          {/* Stock Lookup */}
-          <Col xs={24} sm={12} lg={12}>
-              <StockLookup />
-          </Col>
+// function App() {
+//   const [range, setRange] = useState("1d");
+//   return (
+//     <Layout>
+//       <Header>
+//       <Title level={2} style={{ color: "white", textAlign: "center", margin: 10 }}>
+//           Stock Trading Platform
+//         </Title>
+//       </Header>
+//       <Content style={{ padding: "50px" }}>
+//         <Row gutter={[20, 20]}>
+//           {/* Stock Lookup */}
+//           <Col xs={24} sm={12} lg={12}>
+//               <StockLookup />
+//           </Col>
 
-          {/* Historical Data Card */}
-          <Col xs={24} sm={12} lg={12}>
-            <HistoricalData range={range} />
-          </Col>
+//           {/* Historical Data Card */}
+//           <Col xs={24} sm={12} lg={12}>
+//             <HistoricalData range={range} />
+//           </Col>
 
-          {/* Buy Stock Card */}
-          <Col xs={24} sm={12} lg={12}>
-            <Card title="Buy Stock" bordered>
-              {/* TODO: Add the logic*/}
-              <BuyStock />
-            </Card>
-          </Col>
+//           {/* Buy Stock Card */}
+//           <Col xs={24} sm={12} lg={12}>
+//             <Card title="Buy Stock" bordered>
+//               {/* TODO: Add the logic*/}
+//               <BuyStock />
+//             </Card>
+//           </Col>
 
-          {/* Sell Stock Card */}
-          <Col xs={24} sm={12} lg={12}>
-            <Card title="Sell Stock" bordered>
-              {/* TODO: Add the logic*/}
-              <SellStock />
-            </Card>
-          </Col>
-        </Row>
-      </Content>
-    </Layout>
-  );
-}
+//           {/* Sell Stock Card */}
+//           <Col xs={24} sm={12} lg={12}>
+//             <Card title="Sell Stock" bordered>
+//               {/* TODO: Add the logic*/}
+//               <SellStock />
+//             </Card>
+//           </Col>
+//         </Row>
+//       </Content>
+//     </Layout>
+//   );
+// }
 
-export default App;
+// export default App;
