aa80b3cb7ceedd3cf2c0134f35e6f91cbb46578e
varuni24 <varunigupta24@gmail.com>
2024-12-08 20:50:54 -0500

    feat: fixed buy/sell error

--- backend/services/portfolio.py
+++ backend/services/portfolio.py
@@ -5,16 +5,16 @@ from services.alpha_vantage import AlphaVantageService
 class PortfolioService:
     def __init__(self):
         self.alpha_vantage = AlphaVantageService()
-    
+
     def calculate_holding_value(self, stock: Stock) -> float:
         """Calculate current value of a stock holding
-        
+
         Args:
             stock: Stock model instance
-            
+
         Returns:
             Current value of the holding based on latest price
         """
         quote = self.alpha_vantage.get_stock_quote(stock.symbol)
         current_price = float(quote['Global Quote']['05. price'])
-        return current_price * stock.quanity
+        return current_price * stock.quantity
