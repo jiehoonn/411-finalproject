636d28226de5b24dbd3af1de989dbfc14bc22a13
Jiehoon Lee <jiehoonn@bu.edu>
2024-12-10 12:53:25 -0500

    chore: backend structure reset

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,18 +1,20 @@
-from app import db, bcrypt
+from app import db
+import bcrypt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)  # Hashed password
-    balance = db.Column(db.Float, default=1000000.0)  # Starting with $1M
+    password = db.Column(db.String(128), nullable=False)
+    balance = db.Column(db.Float, default=1000000.0)  # Starting balance $1M
     portfolio = db.relationship('Portfolio', backref='user', lazy=True)
 
-    def set_password(self, plaintext_password):
-        self.password = bcrypt.generate_password_hash(plaintext_password).decode('utf-8')
+    def set_password(self, password):
+        salt = bcrypt.gensalt()
+        self.password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-    def check_password(self, plaintext_password):
-        return bcrypt.check_password_hash(self.password, plaintext_password)
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password)
 
 class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
