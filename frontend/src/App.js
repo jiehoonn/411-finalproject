172326a399dc70b228ec8a94ebfcc067c55d7f44
Teresa Joseoh <teresaj@bu.edu>
2024-12-09 22:19:54 -0500

    chore: transfer App.js from teresa branch to teresa2 branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,91 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/dashboard';
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Dashboard />} /> {/* Redirect home to dashboard */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
+
+// import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import StockLookup from './components/stockLookup';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <Routes>
+//           <Route path="/lookup-stock" element={<StockLookup />} />
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+// import React, { useState } from "react";
+// import { Layout, Card, Typography, Row, Col } from "antd";
+// import HistoricalData from "./components/historicalData";
+// import StockLookup from "./components/stockLookup";
+// import BuyStock from "./components/buyStock";
+// import SellStock from "./components/sellStock";
+
+// const { Title } = Typography;
+// const { Header, Content } = Layout;
+
+// function App() {
+//   const [range, setRange] = useState("1d");
+//   return (
+//     <Layout>
+//       <Header>
+//       <Title level={2} style={{ color: "white", textAlign: "center", margin: 10 }}>
+//           Stock Trading Platform
+//         </Title>
+//       </Header>
+//       <Content style={{ padding: "50px" }}>
+//         <Row gutter={[20, 20]}>
+//           {/* Stock Lookup */}
+//           <Col xs={24} sm={12} lg={12}>
+//               <StockLookup />
+//           </Col>
+
+//           {/* Historical Data Card */}
+//           <Col xs={24} sm={12} lg={12}>
+//             <HistoricalData range={range} />
+//           </Col>
+
+//           {/* Buy Stock Card */}
+//           <Col xs={24} sm={12} lg={12}>
+//             <Card title="Buy Stock" bordered>
+//               {/* TODO: Add the logic*/}
+//               <BuyStock />
+//             </Card>
+//           </Col>
+
+//           {/* Sell Stock Card */}
+//           <Col xs={24} sm={12} lg={12}>
+//             <Card title="Sell Stock" bordered>
+//               {/* TODO: Add the logic*/}
+//               <SellStock />
+//             </Card>
+//           </Col>
+//         </Row>
+//       </Content>
+//     </Layout>
+//   );
+// }
+
+// export default App;
