ef1096bacc030150764086bb03dec59c680d03b0
Teresa Joseoh <teresaj@bu.edu>
2024-12-10 03:38:01 -0500

    refactor(database): move SQL initialization into database.py

---
+++ app/backend/database.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# create a SQLAlchemy instance
+db = SQLAlchemy()
+
+def init_db(app):
+    
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
